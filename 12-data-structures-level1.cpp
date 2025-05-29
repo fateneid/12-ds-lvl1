31c7220061d02ce3af702b4ba659a0f98b69d458
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 05:46:32 +0300

    remove unnecessary lines and comments

--- 12-data-structures-level1.cpp
+++ 12-data-structures-level1.cpp
@@ -1,13 +1,4 @@
 #include <iostream>
-#include "STL_Stack.h"
-#include "Stack_Swap.h"
-#include "STL_Queue.h"
-#include "Queue_Swap.h"
-#include "Singly_Linked_List.h"
-#include "Singly_Linked_List_Operations.h"
-#include "Doubly_Linked_List.h"
-#include "Doubly_Linked_List_Operations.h"
-#include "Map.h"
 #include "Union.h"
 using namespace std;
 
